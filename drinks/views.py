4e2ec3d1204f87a1c859521bc5962a12a6a3b6b3
mohametalmeari <mohamet.almeari@gmail.com>
2024-02-22 15:04:27 +0300

    Replace JsonResponse with Response for better display

--- drinks/views.py
+++ drinks/views.py
@@ -1,4 +1,3 @@
-from django.http import JsonResponse
 from .models import Drink
 from .serializers import DrinkSerializer
 from rest_framework.decorators import api_view
@@ -10,7 +9,7 @@ def drink_list(request):
   if request.method == 'GET':
     drinks = Drink.objects.all()
     serializer = DrinkSerializer(drinks, many=True)
-    return JsonResponse({"drinks": serializer.data}, safe=False)
+    return Response({"drinks": serializer.data})
   
   if request.method == 'POST':
     serializer = DrinkSerializer(data=request.data)
