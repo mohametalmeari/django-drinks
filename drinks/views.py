cf5033cc6a6d2065d045cd343a81c74daac80463
mohametalmeari <mohamet.almeari@gmail.com>
2024-02-22 15:01:25 +0300

    Add drink_detail view to handle GET, PUT, and DELETE requests

--- drinks/views.py
+++ drinks/views.py
@@ -17,3 +17,25 @@ def drink_list(request):
     if serializer.is_valid():
       serializer.save()
       return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])  
+def drink_detail(request, id):
+  try:
+    drink = Drink.objects.get(pk=id)
+  except Drink.DoesNotExist:
+    return Response(status=status.HTTP_404_NOT_FOUND)
+  
+  if request.method == 'GET':
+    serializer = DrinkSerializer(drink)
+    return Response(serializer.data)
+  elif request.method == 'PUT':
+    serializer = DrinkSerializer(drink, data=request.data)
+    if serializer.is_valid():
+      serializer.save()
+      return Response(serializer.data)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    pass
+
+  elif request.method == 'DELETE':
+    drink.delete()
+    return Response(status=status.HTTP_204_NO_CONTENT)
